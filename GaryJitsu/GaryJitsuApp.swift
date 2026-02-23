cd44be2530de5f2b02735a6187fae8bab431680d
Tom K <tomknighton@icloud.com>
2026-02-23 22:26:55 +0000

    Initial Commit

---
+++ GaryJitsu/GaryJitsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaryJitsuApp.swift
+//  GaryJitsu
+//
+//  Created by Tom Knighton on 23/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GaryJitsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GaryJitsuTests/GaryJitsuTests.swift
@@ -0,0 +1,17 @@
+//
+//  GaryJitsuTests.swift
+//  GaryJitsuTests
+//
+//  Created by Tom Knighton on 23/02/2026.
+//
+
+import Testing
+@testable import GaryJitsu
+
+struct GaryJitsuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
